25531303d44bc81e06bd4df397860297ee2c3126
Ay-dotcode <joseyowolabi@gmail.com>
2024-08-24 18:14:04 +0300

    styling Card Component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,9 +2,9 @@ import profilePic from '../assets/Bear.jpg';
 function Card(){
     return(
         <div className="card">
-            <img src={profilePic} alt="profile picture" width={176}/>
-            <h2>Owolabi Joseph</h2>
-            <p>I&#39;m a student, i&#39;m studying software engineering & play video games</p>
+            <img className='card-image' src={profilePic} alt="profile picture" width={176}/>
+            <h2 className='card-title'>Owolabi Joseph</h2>
+            <p className='card-text'>I&#39;m a student, i&#39;m studying software engineering & play video games</p>
         </div>
     )
 }
