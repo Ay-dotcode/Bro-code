60881f58940344e70a3a67d845246895afc8752f
Ay-dotcode <joseyowolabi@gmail.com>
2024-08-24 17:43:00 +0300

    getting ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
-import Food from "./components/Food"
-import Footer from "./components/Footer"
-import Header from "./components/Header"
-
 function App() {
 
   return (
     <>
-      <Header />
-      <Food />
-      <Footer />
+    
     </>
   )
 }
